fe5e958dc472c62a04307a417941459fa30e8f56
Yoosup Song <Youssupsong@gmail.com>
2024-06-03 21:09:54 -0400

    Basic Vue Setup with Tailwind and Daisy UI

---
+++ frontend/client/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue'
+import './assets/main.css'
+</script>
+
+<template>
+  <header>
+      <nav>
+        <Navbar />
+      </nav>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
